d934ad2ff4686d5f2f0b737141d8f3d44dce9c49
jaimesebas21 <jaimesebas21@gmail.com>
2019-09-07 12:44:55 -0500

    Ejercicio 1 profesional 3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("--------------PROGRAMA PARA DOS NUMEROS--------------------");
+            Console.WriteLine("DIGITE NUMERO:");
+            numero= int.Parse(Console.ReadLine());
+            if (numero > 9){
+                Console.WriteLine("DIGITE NUMERO: "+numero+" Tiene dos digitos");
+                
+            }
+            else if (numero>=100){
+                Console.WriteLine("DIGITE NUMERO: "+numero+" Tiene tres digitos");
+            }
+            else{
+                Console.WriteLine("DIGITE NUMERO: "+numero+" No Tiene dos digitos");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
